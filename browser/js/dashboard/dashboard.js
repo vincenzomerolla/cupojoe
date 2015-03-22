3a3f84a581905be0da2d25f8a236e0f306bd6324
Colin VanLang <colinvanlang@gmail.com>
2015-03-22 10:02:22 -0400

    savepoint

--- browser/js/dashboard/dashboard.js
+++ browser/js/dashboard/dashboard.js
@@ -1,1 +1,12 @@
-app.
+'use strict';
+app.config(function($stateProvider) {
+  $stateProvider.state('dashboard', {
+    url: '/dashboard',
+    templateUrl: 'js/dashboard/dashboard.html',
+    controller: 'DashboardCtrl'
+  });
+});
+
+app.controller('DashboardCtrl', function($scope) {
+
+});
