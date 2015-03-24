304ef35d8869f2d96405ba82eb30f56b749a27c5
Colin VanLang <colinvanlang@gmail.com>
2015-03-24 12:23:28 -0400

    Changes made to models

--- server/db/models/result.js
+++ server/db/models/result.js
@@ -1,10 +1,13 @@
 'use strict';
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var File = mongoose.model('File');
 
 var schema = new Schema({
   score: { type: Number },
   output: { type: String },
+  publicFiles: [File.schema],
+  status: { type: String, enum: ['Started', 'Submitted'] },
   test: { type: Schema.Types.ObjectId, ref: 'Test' },
   user: { type: Schema.Types.ObjectId, ref: 'User' }  
 });
