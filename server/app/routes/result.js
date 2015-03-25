37a92ce10ba307b43bc298e4e47a5cb618a2d669
Colin VanLang <colinvanlang@gmail.com>
2015-03-25 18:44:14 -0400

    savepoint

--- server/app/routes/result.js
+++ server/app/routes/result.js
@@ -95,7 +95,8 @@ var runOnDocker = function(result) {
         resolve(result);
       });
     } else {
-      resolve('\n\n\033[1;33;40m 33;40  \033[1;33;41m 33;41  \033[1;33;42m 33;42  \033[1;33;43m 33;43  \033[1;33;44m 33;44  \033[1;33;45m 33;45  \033[1;33;46m 33;46  \033[1m\033[0\n\n\033[1;33;42m >> Tests OK\n\n');
+      result.output = 'This is a test environment';
+      resolve(result);
     }
   });
 };
@@ -103,10 +104,10 @@ var runOnDocker = function(result) {
 router.route('/:id/run')
   .get(function(req, res, next) {
     runOnDocker(req.data).then(function(result) {
-      // result.save(function(err, savedResult) {
-      //   if (err) return next(err);
-      //   res.json(savedResult);
-      // });
-      res.json({output: result});
+      result.save(function(err, savedResult) {
+        if (err) return next(err);
+        res.json(savedResult);
+      });
+      // res.json({output: result});
     });
   });
