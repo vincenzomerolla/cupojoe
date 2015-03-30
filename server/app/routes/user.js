8a4325c8b68141947874fed62cc359c6b638a463
covlllp <covlllp@users.noreply.github.com>
2015-03-30 18:53:32 -0400

    Update user.js

--- server/app/routes/user.js
+++ server/app/routes/user.js
@@ -75,7 +75,6 @@ router.get('/:id/test', function(req, res, next) {
 });
 router.get('/:id/takenTests', function(req, res, next) {
   User.populate(req.data, 'takenTests').then(function(user) {
-    console.log(user.takenTests.length);
     res.json(user.takenTests);
   });
 });
